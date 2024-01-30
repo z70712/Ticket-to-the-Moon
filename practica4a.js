0f319b28bd8d5f1c8325f1966eedb677e0f7407a
David <157492971+z70712@users.noreply.github.com>
2024-01-30 16:50:56 +0100

    Add files via upload

---
+++ practica4a.js
@@ -0,0 +1,12 @@
+	var seconds = 0;
+			function timer(){
+				seconds = seconds + 1;
+				document.getElementById("counter").innerHTML = seconds;
+			}
+			setInterval(timer, 1000);/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
